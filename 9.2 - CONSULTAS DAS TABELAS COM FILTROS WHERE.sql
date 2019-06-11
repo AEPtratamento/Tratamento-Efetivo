a2b9a38afd11031709333cc60c7ecc8b94a11011
Pedro Henrique <33032044+Pedro-Henrique-Moura@users.noreply.github.com>
2019-06-11 09:29:41 -0300

    Add files via upload

---
+++ 9.2 - CONSULTAS DAS TABELAS COM FILTROS WHERE.sql
@@ -0,0 +1,4 @@
+SELECT * FROM MEDICO WHERE cod_medico = 21;
+SELECT nome_paciente,peso,altura FROM PACIENTE WHERE cod_paciente = 4;
+SELECT cod_paciente,ini_trat,fim_trat FROM TRATAMENTO WHERE cod_tratamento = 1;
+SELECT nome_medico,crm FROM MEDICO WHERE cod_estado = 27;
