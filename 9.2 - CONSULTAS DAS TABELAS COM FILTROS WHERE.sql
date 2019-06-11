4ef9e0f3416b378427d944139eec0a6a09a82e03
Pedro Henrique <33032044+Pedro-Henrique-Moura@users.noreply.github.com>
2019-06-11 17:49:49 -0300

    Delete 9.2 - CONSULTAS DAS TABELAS COM FILTROS WHERE.sql
    Mudei de lugar,to excluindo

--- 9.2 - CONSULTAS DAS TABELAS COM FILTROS WHERE.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM MEDICO WHERE cod_medico = 21;
-SELECT nome_paciente,peso,altura FROM PACIENTE WHERE cod_paciente = 4;
-SELECT cod_paciente,ini_trat,fim_trat FROM TRATAMENTO WHERE cod_tratamento = 1;
-SELECT nome_medico,crm FROM MEDICO WHERE cod_estado = 27;
